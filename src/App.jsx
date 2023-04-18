a51cc5714729e5950f19d762419b046fa81cbdfa
GyanendraMohan <gmpatel152@gmail.com>
2023-04-18 14:08:53 +0530

    setup apis and folder structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,28 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
+import { fetchDataFromApi } from "./utils/api"
 import './App.css'
+import { useDispatch, useSelector } from 'react-redux';
+import { getApiConfiguration } from './store/homeSlice';
 function App() {
+  const dispatch = useDispatch()
+  const { url
+  } = useSelector((state) => state.home);
+  console.log(url);
 
+  useEffect(() => {
+    apiTesting();
+  }, [])
+
+  const apiTesting = () => {
+    fetchDataFromApi('/movie/popular')
+      .then((res) => {
+        console.log(res);
+        dispatch(getApiConfiguration(res))
+      })
+  }
 
   return (
-    <div className="App">
-    </div>
+    <div className="App" style={{ color: "white" }}>App {url?.total_pages}</div>
   )
 }
 
---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import "./styes.scss";
+
+const Footer = () => {
+    return (
+        <div>Footer</div>
+    )
+}
+
+export default Footer
