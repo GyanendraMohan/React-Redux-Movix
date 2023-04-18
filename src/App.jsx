b9f1bedde878a228f17b4826a3e513f49a51735c
GyanendraMohan <gmpatel152@gmail.com>
2023-04-18 18:47:40 +0530

    setup the api and endpoints

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,16 @@
 import { useEffect, useState } from 'react'
+import { BrowserRouter, Routes, Route } from "react-router-dom"
 import { fetchDataFromApi } from "./utils/api"
 import './App.css'
 import { useDispatch, useSelector } from 'react-redux';
 import { getApiConfiguration } from './store/homeSlice';
+import Home from './pages/home/Home';
+import Details from './pages/details/Details';
+import SearchResult from './pages/searchResult/SearchResult';
+import Explore from './pages/explore/Explore';
+import PageNotFound from './pages/404/PageNotFound';
+// import Header from './components/header/Header';
+// import Footer from './components/footer/Footer';
 function App() {
   const dispatch = useDispatch()
   const { url
@@ -10,19 +18,36 @@ function App() {
   console.log(url);
 
   useEffect(() => {
-    apiTesting();
+    fetchApiConfig();
   }, [])
 
-  const apiTesting = () => {
-    fetchDataFromApi('/movie/popular')
+  const fetchApiConfig = () => {
+    fetchDataFromApi('/configuration')
       .then((res) => {
         console.log(res);
-        dispatch(getApiConfiguration(res))
+
+        const url = {
+          backdrop: res.images.secure_base_url + "original",
+          poster: res.images.secure_base_url + "original",
+          profile: res.images.secure_base_url + "original",
+        }
+
+        dispatch(getApiConfiguration(url))
       })
   }
 
   return (
-    <div className="App" style={{ color: "white" }}>App {url?.total_pages}</div>
+    <BrowserRouter>
+      {/* <Header /> */}
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/:mediaType/:id" element={<Details />} />
+        <Route path="/search/:query" element={<SearchResult />} />
+        <Route path="/explore/:mediaType" element={<Explore />} />
+        <Route path="*" element={<PageNotFound />} />
+      </Routes>
+      {/* <Footer /> */}
+    </BrowserRouter>
   )
 }
 
--- src/pages/home/heroBanner/HeroBanner.jsx
+++ src/pages/home/heroBanner/HeroBanner.jsx
@@ -1,10 +1,60 @@
-import React from 'react'
-import "./styles.scss"
+import React, { useEffect, useState } from 'react'
+import "./styles.scss";
+
+import { useNavigate } from "react-router-dom"
+import useFetch from '../../../hooks/useFetch';
+import { useSelector } from 'react-redux';
+import Img from '../../../components/lazyLoadImage/img';
+import ContentWrapper from '../../../components/contentWrapper/ContentWrapper';
 
 const HeroBanner = () => {
+
+    const [background, setBackground] = useState("");
+    const [query, setQuery] = useState("");
+    const navigate = useNavigate();
+    const { url } = useSelector((state) => state.home);
+    const { data, loading } = useFetch("/movie/upcoming");
+
+    const searchQueryHandler = (event) => {
+        if (event.key === "Enter" && query.length > 0) {
+            navigate(`/search/${query}`);
+        }
+    }
+
+    useEffect(() => {
+
+        const bg = url.backdrop + data?.results?.[Math.floor(Math.random() * 20)]?.backdrop_path;
+        setBackground(bg)
+    }, [data])
+
     return (
-        <div>HeroBanner</div>
+        <div className="heroBanner">
+            {!loading && <div className="backdrop-img">
+                <Img src={background} />
+            </div>}
+
+            <ContentWrapper>
+                <div className="heroBannerContent">
+                    <span className="title">
+                        Welcome
+                    </span>
+                    <span className="subTitle">
+                        Millions of Movies, TV shows and people to discover.
+                        Explore now.
+                    </span>
+                    <div className="searchInput">
+                        <input
+                            type="text"
+                            placeholder='Search for a movie or tv show...'
+                            onChange={(e) => setQuery(e.target.value)}
+                            onKeyUp={searchQueryHandler}
+                        />
+                        <button>Search</button>
+                    </div>
+                </div>
+            </ContentWrapper>
+        </div>
     )
 }
 
-export default HeroBanner
+export default HeroBanner;
