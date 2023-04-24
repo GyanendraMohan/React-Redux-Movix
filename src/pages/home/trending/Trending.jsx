a05506da182077ec0db043d611baf2e84d49f117
GyanendraMohan <gmpatel152@gmail.com>
2023-04-24 22:19:53 +0530

    created carousel for trending section

--- src/pages/home/trending/Trending.jsx
+++ src/pages/home/trending/Trending.jsx
@@ -3,6 +3,7 @@ import ContentWrapper from '../../../components/contentWrapper/ContentWrapper'
 import "../style.scss"
 import SwitchTabs from '../../../components/switchTabs/SwitchTabs'
 import useFetch from '../../../hooks/useFetch'
+import Carousel from '../../../components/carousel/Carousel'
 
 const Trending = () => {
 
@@ -22,6 +23,7 @@ const Trending = () => {
                 </span>
                 <SwitchTabs data={["Day", "Week"]} onTabChange={onTabChange} />
             </ContentWrapper>
+            <Carousel data={data?.results} loading={loading} />
         </div>
     )
 }
